913975f74849a41c53ee3ff39697f76e4c65ead6
Raminder Singh <romi_ssk@yahoo.co.in>
2025-06-25 16:33:57 +0530

    use text type to save sync_done_lsn

--- replicator/migrations/20250616122411_create_replication_state_table.sql
+++ replicator/migrations/20250616122411_create_replication_state_table.sql
@@ -12,6 +12,9 @@ create table
         pipeline_id bigint not null,
         table_id oid not null,
         state table_state not null,
-        sync_done_lsn pg_lsn null,
+        -- TODO: should use the pg_lsn type here but simplifying because sqlx doen't yet support a PgLsn type
+        -- and we'd have to implement it manually (and the Encode/Decode) traits to make it work. So taking
+        -- the simpler approach of using text type for now.
+        sync_done_lsn text null,
         primary key (pipeline_id, table_id)
     );
