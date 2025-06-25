b5ee112a1d406fa05185aa7ad4c2ebbaa5f6da28
Raminder Singh <romi_ssk@yahoo.co.in>
2025-06-25 17:43:42 +0530

    add more comments

--- etl/src/v2/state/store/base.rs
+++ etl/src/v2/state/store/base.rs
@@ -42,13 +42,17 @@ pub trait StateStore {
         table_id: TableId,
     ) -> impl Future<Output = Result<Option<TableReplicationPhase>, StateStoreError>> + Send;
 
-    /// Return the table replication states for all the tables from the cache.
+    /// Returns the table replication states for all the tables from the cache.
     /// Does not read from the persistent store.
     fn get_table_replication_states(
         &self,
     ) -> impl Future<Output = Result<HashMap<TableId, TableReplicationPhase>, StateStoreError>> + Send;
 
     /// Loads the table replication states from the persistent state into the cache.
+    /// This should called once at program start to load the state into the cache
+    /// and then use only the `get_X` methods to access the state. Updating the state
+    /// by calling the `update_table_replication_state` updates in both the cache and
+    /// the persistent store, so no need to ever load the state again.
     fn load_table_replication_states(
         &self,
     ) -> impl Future<Output = Result<usize, StateStoreError>> + Send;
