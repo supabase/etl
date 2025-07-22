b7610b79d22c1c84c42504da319f1462ef912e45
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-07-22 13:19:43 +0200

    Improve

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -3,6 +3,7 @@ pub mod concurrency;
 pub mod conversions;
 pub mod destination;
 pub mod encryption;
+pub mod failpoints;
 pub mod pipeline;
 pub mod replication;
 pub mod schema;
@@ -10,5 +11,3 @@ pub mod state;
 #[cfg(any(test, feature = "test-utils"))]
 pub mod test_utils;
 pub mod workers;
-
-pub use tokio_postgres::config::SslMode;
--- etl/tests/failpoints/pipeline_test.rs
+++ etl/tests/failpoints/pipeline_test.rs
@@ -1,33 +1,33 @@
 use etl::destination::memory::MemoryDestination;
+use etl::failpoints::START_TABLE_SYNC_AFTER_DATA_SYNC;
 use etl::pipeline::{PipelineError, PipelineId};
 use etl::state::store::notify::NotifyingStateStore;
 use etl::state::table::TableReplicationPhaseType;
 use etl::workers::base::WorkerWaitError;
-use postgres::schema::ColumnSchema;
-use postgres::tokio::test_utils::TableModification;
+use fail::FailScenario;
 use rand::random;
 use telemetry::init_test_tracing;
-use tokio_postgres::types::Type;
 
 use etl::test_utils::database::spawn_database;
 use etl::test_utils::pipeline::create_pipeline;
-use etl::test_utils::state_store::{FaultConfig, FaultInjectingStateStore, FaultType};
 use etl::test_utils::test_destination_wrapper::TestDestinationWrapper;
 use etl::test_utils::test_schema::{TableSelection, setup_test_database_schema};
 
-// TODO: find a way to inject errors in a way that is predictable.
-#[ignore]
+/*
+Tests that we want to add:
+TBD
+ */
+
 #[tokio::test(flavor = "multi_thread")]
-async fn pipeline_handles_table_sync_worker_panic() {
+async fn pipeline_handles_table_sync_worker_panic_during_data_sync() {
+    let scenario = FailScenario::setup();
+    fail::cfg(START_TABLE_SYNC_AFTER_DATA_SYNC, "panic").unwrap();
+
     init_test_tracing();
     let database = spawn_database().await;
     let database_schema = setup_test_database_schema(&database, TableSelection::Both).await;
 
-    let fault_config = FaultConfig {
-        store_table_replication_state: Some(FaultType::Panic),
-        ..Default::default()
-    };
-    let state_store = FaultInjectingStateStore::wrap(NotifyingStateStore::new(), fault_config);
+    let state_store = NotifyingStateStore::new();
     let destination = TestDestinationWrapper::wrap(MemoryDestination::new());
 
     // We start the pipeline from scratch.
@@ -42,14 +42,12 @@ async fn pipeline_handles_table_sync_worker_panic() {
 
     // We register the interest in waiting for both table syncs to have started.
     let users_state_notify = state_store
-        .get_inner()
         .notify_on_table_state(
             database_schema.users_schema().id,
             TableReplicationPhaseType::DataSync,
         )
         .await;
     let orders_state_notify = state_store
-        .get_inner()
         .notify_on_table_state(
             database_schema.orders_schema().id,
             TableReplicationPhaseType::DataSync,
@@ -74,20 +72,20 @@ async fn pipeline_handles_table_sync_worker_panic() {
         }
         other => panic!("Expected TableSyncWorkersFailed error, but got: {other:?}"),
     }
+
+    scenario.teardown();
 }
 
-// TODO: find a way to inject errors in a way that is predictable.
+// TODO: inject the failure via fail-rs.
 #[ignore]
 #[tokio::test(flavor = "multi_thread")]
 async fn pipeline_handles_table_sync_worker_error() {
+    let scenario = FailScenario::setup();
     init_test_tracing();
     let database = spawn_database().await;
     let database_schema = setup_test_database_schema(&database, TableSelection::Both).await;
 
-    let fault_config = FaultConfig {
-        ..Default::default()
-    };
-    let state_store = FaultInjectingStateStore::wrap(NotifyingStateStore::new(), fault_config);
+    let state_store = NotifyingStateStore::new();
     let destination = TestDestinationWrapper::wrap(MemoryDestination::new());
 
     // We start the pipeline from scratch.
@@ -102,14 +100,12 @@ async fn pipeline_handles_table_sync_worker_error() {
 
     // Register notifications for when table sync is started.
     let users_state_notify = state_store
-        .get_inner()
         .notify_on_table_state(
             database_schema.users_schema().id,
             TableReplicationPhaseType::DataSync,
         )
         .await;
     let orders_state_notify = state_store
-        .get_inner()
         .notify_on_table_state(
             database_schema.orders_schema().id,
             TableReplicationPhaseType::DataSync,
@@ -134,12 +130,15 @@ async fn pipeline_handles_table_sync_worker_error() {
         }
         other => panic!("Expected TableSyncWorkersFailed error, but got: {other:?}"),
     }
+
+    scenario.teardown();
 }
 
-// TODO: find a way to inject errors in a way that is predictable.
+// TODO: inject the failure via fail-rs.
 #[ignore]
 #[tokio::test(flavor = "multi_thread")]
 async fn table_schema_copy_retries_after_data_sync_failure() {
+    let scenario = FailScenario::setup();
     init_test_tracing();
     let database = spawn_database().await;
     let database_schema = setup_test_database_schema(&database, TableSelection::Both).await;
@@ -147,32 +146,24 @@ async fn table_schema_copy_retries_after_data_sync_failure() {
     let state_store = NotifyingStateStore::new();
     let destination = TestDestinationWrapper::wrap(MemoryDestination::new());
 
-    // Configure state store to fail during data sync.
-    let fault_config = FaultConfig {
-        ..Default::default()
-    };
-    let failing_state_store = FaultInjectingStateStore::wrap(state_store.clone(), fault_config);
-
     // We start the pipeline from scratch.
     let pipeline_id: PipelineId = random();
     let mut pipeline = create_pipeline(
         &database.config,
         pipeline_id,
         database_schema.publication_name(),
-        failing_state_store.clone(),
+        state_store.clone(),
         destination.clone(),
     );
 
     // Register notifications for table sync phases.
-    let users_state_notify = failing_state_store
-        .get_inner()
+    let users_state_notify = state_store
         .notify_on_table_state(
             database_schema.users_schema().id,
             TableReplicationPhaseType::DataSync,
         )
         .await;
-    let orders_state_notify = failing_state_store
-        .get_inner()
+    let orders_state_notify = state_store
         .notify_on_table_state(
             database_schema.orders_schema().id,
             TableReplicationPhaseType::DataSync,
@@ -247,113 +238,6 @@ async fn table_schema_copy_retries_after_data_sync_failure() {
     assert_eq!(table_schemas.len(), 2);
     assert_eq!(table_schemas[0], database_schema.orders_schema());
     assert_eq!(table_schemas[1], database_schema.users_schema());
-}
 
-#[tokio::test(flavor = "multi_thread")]
-async fn table_schema_copy_retries_after_finished_copy_failure() {
-    init_test_tracing();
-    let database = spawn_database().await;
-    let database_schema = setup_test_database_schema(&database, TableSelection::Both).await;
-
-    let state_store = NotifyingStateStore::new();
-    let destination = TestDestinationWrapper::wrap(MemoryDestination::new());
-
-    // We start the pipeline from scratch.
-    let pipeline_id: PipelineId = random();
-    let mut pipeline = create_pipeline(
-        &database.config,
-        pipeline_id,
-        database_schema.publication_name(),
-        state_store.clone(),
-        destination.clone(),
-    );
-
-    // We wait for two table schemas to be received.
-    let schemas_notify = destination.wait_for_n_schemas(2).await;
-    // We wait for both table states to be in sync done.
-    let users_state_notify = state_store
-        .notify_on_table_state(
-            database_schema.users_schema().id,
-            TableReplicationPhaseType::SyncDone,
-        )
-        .await;
-    let orders_state_notify = state_store
-        .notify_on_table_state(
-            database_schema.orders_schema().id,
-            TableReplicationPhaseType::SyncDone,
-        )
-        .await;
-
-    pipeline.start().await.unwrap();
-
-    schemas_notify.notified().await;
-    users_state_notify.notified().await;
-    orders_state_notify.notified().await;
-
-    pipeline.shutdown_and_wait().await.unwrap();
-
-    // We check that the states are correctly set.
-    let table_replication_states = state_store.get_table_replication_states().await;
-    assert_eq!(table_replication_states.len(), 2);
-    assert_eq!(
-        table_replication_states
-            .get(&database_schema.users_schema().id)
-            .unwrap()
-            .as_type(),
-        TableReplicationPhaseType::SyncDone
-    );
-    assert_eq!(
-        table_replication_states
-            .get(&database_schema.orders_schema().id)
-            .unwrap()
-            .as_type(),
-        TableReplicationPhaseType::SyncDone
-    );
-
-    // We check that the table schemas have been stored.
-    let table_schemas = destination.get_table_schemas().await;
-    assert_eq!(table_schemas.len(), 2);
-    assert_eq!(table_schemas[0], database_schema.orders_schema());
-    assert_eq!(table_schemas[1], database_schema.users_schema());
-
-    // We assume now that the schema of a table changes before sync done is performed.
-    database
-        .alter_table(
-            database_schema.orders_schema().name.clone(),
-            &[TableModification::AddColumn {
-                name: "date",
-                data_type: "integer",
-            }],
-        )
-        .await
-        .unwrap();
-    let mut extended_orders_table_schema = database_schema.orders_schema().clone();
-    extended_orders_table_schema
-        .column_schemas
-        .push(ColumnSchema {
-            name: "date".to_string(),
-            typ: Type::INT4,
-            modifier: -1,
-            nullable: true,
-            primary: false,
-        });
-
-    // We recreate a pipeline, assuming the other one was stopped, using the same state and destination.
-    let mut pipeline = create_pipeline(
-        &database.config,
-        pipeline_id,
-        database_schema.publication_name(),
-        state_store.clone(),
-        destination.clone(),
-    );
-
-    pipeline.start().await.unwrap();
-
-    pipeline.shutdown_and_wait().await.unwrap();
-
-    // We check that the table schemas haven't changed.
-    let table_schemas = destination.get_table_schemas().await;
-    assert_eq!(table_schemas.len(), 2);
-    assert_eq!(table_schemas[0], database_schema.orders_schema());
-    assert_eq!(table_schemas[1], database_schema.users_schema());
+    scenario.teardown();
 }
