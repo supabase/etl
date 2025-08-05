3e619987259b1357761678e13e8ec1e9de53074c
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-08-05 10:57:36 +0200

    Improve

--- api/src/routes/pipelines.rs
+++ api/src/routes/pipelines.rs
@@ -883,11 +883,9 @@ pub async fn rollback_table_state(
         .ok_or(PipelineError::MissingTableReplicationState)?;
 
     // Check if the current state is rollbackable (has ManualRetry policy)
-    let current_state = current_row
-        .deserialize_metadata()
-        .unwrap().unwrap();
-        // .map_err(PipelineError::InvalidTableReplicationState)?
-        // .ok_or(PipelineError::MissingTableReplicationState)?;
+    let current_state = current_row.deserialize_metadata().unwrap().unwrap();
+    // .map_err(PipelineError::InvalidTableReplicationState)?
+    // .ok_or(PipelineError::MissingTableReplicationState)?;
     if !current_state.supports_manual_retry() {
         return Err(PipelineError::NotRollbackable(
             "Only manual retry errors can be rolled back".to_string(),
