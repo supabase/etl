34ed8f0061ceaf0e099cff7405c480ec80560830
Raminder Singh <romi_ssk@yahoo.co.in>
2025-08-12 16:45:03 +0530

    run upkeep task

--- etl-api/src/metrics.rs
+++ etl-api/src/metrics.rs
@@ -1,6 +1,25 @@
+use std::time::Duration;
+
 use metrics_exporter_prometheus::{BuildError, PrometheusBuilder, PrometheusHandle};
+use tracing::trace;
 
 pub fn init_metrics() -> Result<PrometheusHandle, BuildError> {
-    // TODO: run upkeep task at regular intervals
-    PrometheusBuilder::new().install_recorder()
+    let builder = PrometheusBuilder::new();
+
+    let handle = builder.install_recorder()?;
+    let handle_clone = handle.clone();
+
+    // This task periodically performs upkeep to avoid unbounded memory growth due to 
+    // metrics collection
+    tokio::spawn(async move {
+        loop {
+            // upkeep_timeout hardcoded for now. Will make it configurable later if it creates a problem
+            let upkeep_timeout = Duration::from_secs(5);
+            tokio::time::sleep(upkeep_timeout).await;
+            trace!("running metrics upkeep");
+            handle_clone.run_upkeep();
+        }
+    });
+
+    Ok(handle)
 }
