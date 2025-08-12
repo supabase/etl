2ecfd1213a1ab4cb75a340aa54cd80fc3dd27184
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-08-12 12:17:31 +0200

    Update

--- etl/src/replication/table_sync.rs
+++ etl/src/replication/table_sync.rs
@@ -12,9 +12,8 @@ use crate::concurrency::signal::SignalTx;
 use crate::concurrency::stream::BatchStream;
 use crate::destination::Destination;
 use crate::error::{ErrorKind, EtlError, EtlResult};
-use crate::failpoints::START_TABLE_SYNC__DURING_DATA_SYNC;
 #[cfg(feature = "failpoints")]
-use crate::failpoints::{START_TABLE_SYNC__AFTER_DATA_SYNC, etl_fail_point};
+use crate::failpoints::{START_TABLE_SYNC__AFTER_DATA_SYNC, START_TABLE_SYNC__DURING_DATA_SYNC, etl_fail_point};
 use crate::replication::client::PgReplicationClient;
 use crate::replication::slot::get_slot_name;
 use crate::replication::stream::TableCopyStream;
