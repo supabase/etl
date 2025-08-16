2a1c89500d095f6bda1a0013ce921cb6391aa4cf
Raminder Singh <romi_ssk@yahoo.co.in>
2025-08-16 11:44:27 +0530

    simplify metric registration

--- etl-replicator/src/metrics.rs
+++ etl-replicator/src/metrics.rs
@@ -1,8 +1,6 @@
-use etl::metrics::register_metrics;
 use etl_telemetry::metrics::init_default_metrics;
 
 pub fn init_metrics() -> anyhow::Result<()> {
     init_default_metrics()?;
-    register_metrics();
     Ok(())
 }
--- etl/src/metrics.rs
+++ etl/src/metrics.rs
@@ -1,30 +1,34 @@
+use std::sync::Once;
+
 use metrics::{Unit, describe_counter, describe_gauge};
+
+static REGISTER_METRICS: Once = Once::new();
 
 pub const ETL_TABLES_TOTAL: &str = "etl_tables_total";
 pub const ETL_TABLE_SYNC_ROWS_COPIED_TOTAL: &str = "etl_table_sync_rows_copied_total";
 pub const ETL_APPLY_EVENTS_COPIED_TOTAL: &str = "etl_apply_events_copied_total";
 
-/// Register metrics emitted by etl. This method should be called
-/// early during initialization by the binary using the etl crate.
-/// If this method is not called during initialization, the metrics
-/// will still be emitted, but they will not have their descriptions
-/// returned with the metrics.
-pub fn register_metrics() {
-    describe_gauge!(
-        ETL_TABLES_TOTAL,
-        Unit::Count,
-        "Total number of tables being copied by etl"
-    );
+/// Register metrics emitted by etl. This should be called before starting a pipeline.
+/// It is safe to call this method multiple times. It is guaraneed to register the
+/// metrics only once.
+pub(crate) fn register_metrics() {
+    REGISTER_METRICS.call_once(|| {
+        describe_gauge!(
+            ETL_TABLES_TOTAL,
+            Unit::Count,
+            "Total number of tables being copied by etl"
+        );
 
-    describe_counter!(
-        ETL_TABLE_SYNC_ROWS_COPIED_TOTAL,
-        Unit::Count,
-        "Total number of rows copied to destination during table sync"
-    );
+        describe_counter!(
+            ETL_TABLE_SYNC_ROWS_COPIED_TOTAL,
+            Unit::Count,
+            "Total number of rows copied to destination during table sync"
+        );
 
-    describe_counter!(
-        ETL_APPLY_EVENTS_COPIED_TOTAL,
-        Unit::Count,
-        "Total number of events copied to destination in apply loop"
-    );
+        describe_counter!(
+            ETL_APPLY_EVENTS_COPIED_TOTAL,
+            Unit::Count,
+            "Total number of events copied to destination in apply loop"
+        );
+    });
 }
