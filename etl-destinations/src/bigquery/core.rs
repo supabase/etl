d544c9e5522dbb7b2cdfebec31a700e987c159c2
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-08-20 10:05:56 +0200

    Improve

--- etl-destinations/src/bigquery/core.rs
+++ etl-destinations/src/bigquery/core.rs
@@ -156,7 +156,6 @@ impl FromStr for SequencedBigQueryTableId {
 }
 
 impl Display for SequencedBigQueryTableId {
-    /// Formats the sequenced table ID as `table_name_sequence`.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}_{}", self.0, self.1)
     }
