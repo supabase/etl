9e98b684fe6155e35440ac72522d8d3ce7f31afb
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-08-26 16:59:29 +0200

    Improve

--- etl-api/src/configs/pipeline.rs
+++ etl-api/src/configs/pipeline.rs
@@ -105,7 +105,9 @@ impl From<FullApiPipelineConfig> for StoredPipelineConfig {
             table_error_retry_delay_ms: value
                 .table_error_retry_delay_ms
                 .unwrap_or(DEFAULT_TABLE_ERROR_RETRY_DELAY_MS),
-            max_table_sync_workers: value.max_table_sync_workers.unwrap_or(DEFAULT_MAX_TABLE_SYNC_WORKERS),
+            max_table_sync_workers: value
+                .max_table_sync_workers
+                .unwrap_or(DEFAULT_MAX_TABLE_SYNC_WORKERS),
         }
     }
 }
@@ -174,7 +176,10 @@ mod tests {
             stored.table_error_retry_delay_ms,
             DEFAULT_TABLE_ERROR_RETRY_DELAY_MS
         );
-        assert_eq!(stored.max_table_sync_workers, 0);
+        assert_eq!(
+            stored.max_table_sync_workers,
+            DEFAULT_MAX_TABLE_SYNC_WORKERS
+        );
     }
 
     #[test]
