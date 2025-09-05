20ec913f63a52fea1fb2f5db5349ffb0dbb1f87a
Raminder Singh <romi_ssk@yahoo.co.in>
2025-09-05 12:38:55 +0530

    merge bq metrics ito core etl ones

--- etl/src/metrics.rs
+++ etl/src/metrics.rs
@@ -12,6 +12,7 @@ pub const MILLIS_PER_SEC: f64 = 1_000.0;
 pub const PHASE: &str = "phase";
 pub const TABLE_SYNC: &str = "table_sync";
 pub const APPLY: &str = "apply";
+pub const DESTINATION: &str = "destination";
 
 /// Register metrics emitted by etl. This should be called before starting a pipeline.
 /// It is safe to call this method multiple times. It is guaraneed to register the
