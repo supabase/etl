bebdcbf9243377e6bae75cb4e9b2b4f5c4999beb
Raminder Singh <romi_ssk@yahoo.co.in>
2025-09-17 11:14:26 +0530

    cleanup in test

--- etl-destinations/tests/iceberg_destination.rs
+++ etl-destinations/tests/iceberg_destination.rs
@@ -1,5 +1,6 @@
 #![cfg(feature = "iceberg")]
 
+use etl::state::table::TableReplicationPhaseType;
 use etl::test_utils::database::spawn_source_database;
 use etl::test_utils::notify::NotifyingStore;
 use etl::test_utils::pipeline::create_pipeline;
@@ -41,6 +42,7 @@ async fn table_copy_and_streaming_with_restart() {
         IcebergClient::new_with_rest_catalog(get_catalog_url(), warehouse_name, create_props());
 
     let namespace = "test_namespace";
+    client.create_namespace_if_missing(namespace).await.unwrap();
 
     let raw_destination =
         IcebergDestination::new(client.clone(), namespace.to_string(), store.clone());
@@ -48,7 +50,7 @@ async fn table_copy_and_streaming_with_restart() {
     let destination = TestDestinationWrapper::wrap(raw_destination);
 
     // Start pipeline from scratch.
-    let _pipeline = create_pipeline(
+    let mut pipeline = create_pipeline(
         &database.config,
         pipeline_id,
         database_schema.publication_name(),
@@ -56,11 +58,44 @@ async fn table_copy_and_streaming_with_restart() {
         destination.clone(),
     );
 
+    // Register notifications for table copy completion.
+    let users_state_notify = store
+        .notify_on_table_state(
+            database_schema.users_schema().id,
+            TableReplicationPhaseType::SyncDone,
+        )
+        .await;
+    let orders_state_notify = store
+        .notify_on_table_state(
+            database_schema.orders_schema().id,
+            TableReplicationPhaseType::SyncDone,
+        )
+        .await;
+
+    pipeline.start().await.unwrap();
+
+    users_state_notify.notified().await;
+    orders_state_notify.notified().await;
+
+    pipeline.shutdown_and_wait().await.unwrap();
+
     // Manual cleanup for now because lakekeeper doesn't allow cascade delete at the warehouse level
     // This feature is planned for future releases. We'll start to use it when it becomes available.
     // The cleanup is not in a Drop impl because each test has different number of object specitic to
     // that test.
-    // client.drop_namespace(namespace).await.unwrap();
+    let users_table = format!(
+        "{}_{}",
+        database_schema.users_schema().name.schema,
+        database_schema.users_schema().name.name
+    );
+    let orders_table = format!(
+        "{}_{}",
+        database_schema.orders_schema().name.schema,
+        database_schema.orders_schema().name.name
+    );
+    client.drop_table(namespace, users_table).await.unwrap();
+    client.drop_table(namespace, orders_table).await.unwrap();
+    client.drop_namespace(namespace).await.unwrap();
     lakekeeper_client
         .drop_warehouse(warehouse_id)
         .await
