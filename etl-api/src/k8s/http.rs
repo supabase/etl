3b8332432d50975a79d3ab3b9b53ebc09801f37d
Raminder Singh <romi_ssk@yahoo.co.in>
2025-10-09 20:02:02 +0530

    factor out create bq secret and add test

--- etl-api/src/k8s/http.rs
+++ etl-api/src/k8s/http.rs
@@ -153,18 +153,10 @@ impl K8sClient for HttpK8sClient {
 
         let encoded_bq_service_account_key = BASE64_STANDARD.encode(bq_service_account_key);
         let secret_name = format!("{prefix}-{BQ_SECRET_NAME_SUFFIX}");
-        let secret_json = json!({
-          "apiVersion": "v1",
-          "kind": "Secret",
-          "metadata": {
-            "name": secret_name,
-            "namespace": DATA_PLANE_NAMESPACE,
-          },
-          "type": "Opaque",
-          "data": {
-            "service-account-key": encoded_bq_service_account_key,
-          }
-        });
+        let secret_json = create_bq_service_account_key_secret_json(
+            &secret_name,
+            &encoded_bq_service_account_key,
+        );
         let secret: Secret = serde_json::from_value(secret_json)?;
 
         let pp = PatchParams::apply(&secret_name);
@@ -627,6 +619,24 @@ fn create_postgres_secret_json(
     })
 }
 
+fn create_bq_service_account_key_secret_json(
+    secret_name: &str,
+    encoded_bq_service_account_key: &str,
+) -> serde_json::Value {
+    json!({
+      "apiVersion": "v1",
+      "kind": "Secret",
+      "metadata": {
+        "name": secret_name,
+        "namespace": DATA_PLANE_NAMESPACE,
+      },
+      "type": "Opaque",
+      "data": {
+        "service-account-key": encoded_bq_service_account_key,
+      }
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -635,9 +645,20 @@ mod tests {
     #[test]
     fn test_create_postgres_secret_json() {
         let secret_name = "test-secret";
-        let encoded_password = "dGVzdC1wYXNzd29yZA==";
+        let encoded_postgres_password = "dGVzdC1wYXNzd29yZA==";
 
-        let secret_json = create_postgres_secret_json(secret_name, encoded_password);
+        let secret_json = create_postgres_secret_json(secret_name, encoded_postgres_password);
+
+        assert_json_snapshot!(secret_json);
+    }
+
+    #[test]
+    fn test_create_bq_service_account_key_secret_json() {
+        let secret_name = "test-secret";
+        let encoded_bq_service_account_key = "ewogICJrZXkiOiAidmFsdWUiCn0=";
+
+        let secret_json =
+            create_bq_service_account_key_secret_json(secret_name, encoded_bq_service_account_key);
 
         assert_json_snapshot!(secret_json);
     }
