93f89a65de9cadec8cf248f4fe5cea827fab327c
Riccardo Busetti <riccardo.busetti@supabase.io>
2025-12-17 08:58:17 +0100

    Improve

--- etl/migrations/20251211000000_destination_tables_metadata.sql
+++ etl/migrations/20251211000000_destination_tables_metadata.sql
@@ -15,7 +15,7 @@ CREATE TABLE etl.destination_tables_metadata (
     -- The name/identifier of the table in the destination system.
     destination_table_id TEXT NOT NULL,
     -- The snapshot_id of the schema currently applied at the destination.
-    snapshot_id BIGINT NOT NULL,
+    snapshot_id PG_LSN NOT NULL,
     -- Status: 'applying' when a schema change is in progress, 'applied' when complete.
     -- If 'applying' is found on startup, recovery may be needed.
     schema_status etl.destination_table_schema_status NOT NULL,
@@ -43,14 +43,14 @@ SELECT
     tm.pipeline_id,
     tm.source_table_id AS table_id,
     tm.destination_table_id,
-    0 AS snapshot_id,
+    '0/0'::pg_lsn AS snapshot_id,
     'applied'::etl.destination_table_schema_status,
     -- Create a bytea of 1s with length equal to the number of columns.
     decode(repeat('01', column_counts.num_columns::int), 'hex') AS replication_mask
 FROM etl.table_mappings tm
 JOIN etl.table_schemas ts ON ts.pipeline_id = tm.pipeline_id
     AND ts.table_id = tm.source_table_id
-    AND ts.snapshot_id = 0
+    AND ts.snapshot_id = '0/0'::pg_lsn
 JOIN (
     SELECT table_schema_id, COUNT(*) AS num_columns
     FROM etl.table_columns
