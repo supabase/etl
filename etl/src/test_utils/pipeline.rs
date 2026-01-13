d44f85201902f09faa5e2f957f7cfc97e27ae65f
Riccardo Busetti <riccardo.busetti@supabase.io>
2026-01-13 15:31:09 +0700

    Update

--- etl/src/test_utils/pipeline.rs
+++ etl/src/test_utils/pipeline.rs
@@ -25,7 +25,7 @@ pub fn test_slot_name(slot_name: &str) -> String {
 ///
 /// # Examples
 ///
-/// ```
+/// ```ignore
 /// // Create a pipeline with default settings
 /// let pipeline = PipelineBuilder::new(pg_config, id, pub_name, store, dest)
 ///     .build();
