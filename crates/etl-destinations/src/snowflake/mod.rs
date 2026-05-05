b2e3b4407733aceb996185826e13257452de7d0f
Victor Farazdagi <simple.square@gmail.com>
2026-05-05 12:55:43 +0300

    update schema encoding

--- crates/etl-destinations/src/snowflake/mod.rs
+++ crates/etl-destinations/src/snowflake/mod.rs
@@ -1,19 +1,23 @@
 mod auth;
 mod channel;
 mod config;
+mod core;
 mod encoding;
 mod error;
+mod metrics;
 mod schema;
 mod sql_client;
-pub mod streaming_client;
+mod streaming_client;
 
 #[cfg(feature = "test-utils")]
 pub mod test_utils;
 
+pub use core::{DefaultSnowflakeDestination, SnowflakeDestination};
+
 pub use auth::{AuthManager, HttpExchanger, TokenExchanger, TokenProvider};
-pub use channel::{ChannelHandle, ChannelStatus};
+pub use channel::{ChannelHandle, ChannelStatus, OffsetToken};
 pub use config::Config;
-pub use encoding::RowBatch;
+pub use encoding::{CdcOperation, RowBatch};
 pub use error::{Error, Result};
 pub use sql_client::SqlClient;
 pub use streaming_client::{
