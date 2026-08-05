9fe106fb9289a57a60728df7ed2f19eb64190913
Jordan McQueen <j@jm.dev>
2026-08-05 17:12:44 +0900

    Use nextest for ClickHouse tests

--- crates/xtask/src/commands/test_clickhouse.rs
+++ crates/xtask/src/commands/test_clickhouse.rs
@@ -286,10 +286,11 @@ fn run_clickhouse_tests(config: &TestClickhouseConfig, sccache: bool) -> Result<
     run_command(cmd, "Failed to run ClickHouse destination tests")
 }
 
-/// Builds Cargo test arguments.
+/// Builds cargo-nextest arguments.
 fn test_args(config: &TestClickhouseConfig) -> Vec<String> {
     let mut args = vec![
-        "test".to_owned(),
+        "nextest".to_owned(),
+        "run".to_owned(),
         "-p".to_owned(),
         config.cargo_package.clone(),
         "--features".to_owned(),
@@ -301,8 +302,6 @@ fn test_args(config: &TestClickhouseConfig) -> Vec<String> {
     if !config.test_name_filter.is_empty() {
         args.push(config.test_name_filter.clone());
     }
-
-    args.extend(["--".to_owned(), "--test-threads=1".to_owned(), "--nocapture".to_owned()]);
     args
 }
 
